409e943ccf175c2670d434c0bcdd93579a66faad
Dustin Sapp <dustin@dustinsapp.com>
2010-09-08 15:50:04 -0400

    Update dashboard chart to display monthly numbers rather than weekly.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @collections = Collection.all(:order => "collected_on")
+    @collections = Collection.find(:all, :select => "month(collected_on) as giving_month, year(collected_on) as giving_year, SUM(amount) as monthly_sum",:group => "month(collected_on), year(collected_on)", :order => "giving_year, giving_month")
+    @mtd_giving = Collection.sum("amount", :conditions => "month(collected_on) = MONTH(NOW())")
+    @last_month_giving = Collection.sum("amount", :conditions => "month(collected_on) = MONTH(NOW())-1")
   end
 
 end
