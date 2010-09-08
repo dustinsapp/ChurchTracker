9dce13caeae431e2d433de1ced5bf16e199bf39d
Dustin Sapp <dustin@dustinsapp.com>
2010-09-08 16:01:26 -0400

    Fix Postgres issue with extracting month and year from date queries.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @collections = Collection.find(:all, :select => "month(collected_on) as giving_month, year(collected_on) as giving_year, SUM(amount) as monthly_sum",:group => "month(collected_on), year(collected_on)", :order => "giving_year, giving_month")
-    @mtd_giving = Collection.sum("amount", :conditions => "month(collected_on) = MONTH(NOW())")
-    @last_month_giving = Collection.sum("amount", :conditions => "month(collected_on) = MONTH(NOW())-1")
+    @collections = Collection.find(:all, :select => "extract(month from collected_on) as giving_month, extract(year from collected_on) as giving_year, SUM(amount) as monthly_sum",:group => "giving_month, giving_year", :order => "giving_year, giving_month")
+    @mtd_giving = Collection.sum("amount", :conditions => "extract(month from collected_on) = extract(month from now())")
+    @last_month_giving = Collection.sum("amount", :conditions => "extract(month from collected_on) = extract(month from now())-1")
   end
 
 end
